add7809a840cc4b8ff8127023fdd94981d45db6c
Ruud van Vliet <ruud@vliet.io>
2026-04-17 11:44:03 +0200

    fix/padnaam

--- pm_backup.sh
+++ pm_backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-FILENAME=~/io.vliet/plusminapp/backup-lcl/pm-backup-lcl-`date +%Y%m%d"-"%H%M%S`.sql
+FILENAME=pm-backup-lcl-`date +%Y%m%d"-"%H%M%S`.sql
 
 docker exec -t pm-database-lcl pg_dump -c -U pm pm > $FILENAME
